9f49bf857f324ea784590ea1ab68fb7f1f79b1e4
Fabio Cunha de Matos <Fabio.Matos@FIPECAFI.DOM>
2018-11-06 20:21:33 -0200

    Django

---
+++ lms-teste/lms_app/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from lms_app.professor import *
+from lms_app.aluno import *
+# Register your models here.
+admin.site.register(Professor)
+admin.site.register(Aluno)
